61636f91f151110a77dfcaa48c2ac1707c1505d7
Sebastina <sebane1@thavea.com>
2026-02-02 07:15:06 -0600

    Feed previews.

--- FacialCameraBroadcaster/MainPage.xaml.cs
+++ FacialCameraBroadcaster/MainPage.xaml.cs
@@ -28,6 +28,11 @@ namespace FacialCameraBroadcaster
         private const int PortLeftEye = 8080;
         private const int PortRightEye = 8081;
         private const int PortMouth = 8082;
+
+        private IDispatcherTimer? _previewTimer;
+        private int _lastLeftFrameLen, _lastRightFrameLen, _lastMouthFrameLen;
+        private bool _leftShowA = true, _rightShowA = true, _mouthShowA = true;
+        private bool _leftPendingSwap, _rightPendingSwap, _mouthPendingSwap;
 #endif
 
         public MainPage()
@@ -36,21 +41,126 @@ namespace FacialCameraBroadcaster
         }
 
 #if ANDROID
-        protected override async void OnAppearing()
+        protected override void OnAppearing()
         {
             base.OnAppearing();
             enumerator = new UsbCameraEnumerator();
-            UsbCameraBroadcastReceiver.UsbDeviceChanged += _ =>
+            UsbCameraBroadcastReceiver.UsbDeviceChanged += OnUsbDeviceChanged;
+            StartPreviewTimer();
+            if (!_loadedOnce)
             {
-                MainThread.BeginInvokeOnMainThread(async () => await LoadUsbCamerasAsync());
-            };
-            // Short delay so the activity is ready for USB permission dialog
-            Loaded += async (_, _) =>
-            {
-                await Task.Delay(500);
-                await LoadUsbCamerasAsync();
-            };
+                _loadedOnce = true;
+                Loaded += async (_, _) =>
+                {
+                    await Task.Delay(500);
+                    await LoadUsbCamerasAsync();
+                };
+            }
         }
+
+        private bool _loadedOnce;
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            UsbCameraBroadcastReceiver.UsbDeviceChanged -= OnUsbDeviceChanged;
+            StopPreviewTimer();
+        }
+
+        private void OnUsbDeviceChanged(Android.Hardware.Usb.UsbDevice _)
+        {
+            MainThread.BeginInvokeOnMainThread(async () => await LoadUsbCamerasAsync());
+        }
+
+        private void StartPreviewTimer()
+        {
+            _previewTimer ??= Application.Current!.Dispatcher.CreateTimer();
+            _previewTimer.Interval = TimeSpan.FromMilliseconds(100);
+            _previewTimer.Tick += (_, _) => UpdatePreviews();
+            _previewTimer.Start();
+        }
+
+        private void StopPreviewTimer()
+        {
+            _previewTimer?.Stop();
+        }
+
+        private void UpdatePreviews()
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                // Swap from previous tick (gives back buffer time to decode)
+                if (_leftPendingSwap) { SwapPreviewVisibility(LeftEyePreviewA, LeftEyePreviewB, ref _leftShowA); _leftPendingSwap = false; }
+                if (_rightPendingSwap) { SwapPreviewVisibility(RightEyePreviewA, RightEyePreviewB, ref _rightShowA); _rightPendingSwap = false; }
+                if (_mouthPendingSwap) { SwapPreviewVisibility(MouthPreviewA, MouthPreviewB, ref _mouthShowA); _mouthPendingSwap = false; }
+            });
+
+            if (leftEyeReader != null)
+            {
+                var frame = leftEyeReader.LatestFrame;
+                if (frame != null && frame.Length > 0 && frame.Length != _lastLeftFrameLen)
+                {
+                    _lastLeftFrameLen = frame.Length;
+                    MainThread.BeginInvokeOnMainThread(() =>
+                    {
+                        SetPreviewBackBuffer(LeftEyePreviewA, LeftEyePreviewB, _leftShowA, frame);
+                        _leftPendingSwap = true;
+                    });
+                }
+            }
+            else
+                _lastLeftFrameLen = 0;
+
+            if (rightEyeReader != null)
+            {
+                var frame = rightEyeReader.LatestFrame;
+                if (frame != null && frame.Length > 0 && frame.Length != _lastRightFrameLen)
+                {
+                    _lastRightFrameLen = frame.Length;
+                    MainThread.BeginInvokeOnMainThread(() =>
+                    {
+                        SetPreviewBackBuffer(RightEyePreviewA, RightEyePreviewB, _rightShowA, frame);
+                        _rightPendingSwap = true;
+                    });
+                }
+            }
+            else
+                _lastRightFrameLen = 0;
+
+            if (mouthReader != null)
+            {
+                var frame = mouthReader.LatestFrame;
+                if (frame != null && frame.Length > 0 && frame.Length != _lastMouthFrameLen)
+                {
+                    _lastMouthFrameLen = frame.Length;
+                    MainThread.BeginInvokeOnMainThread(() =>
+                    {
+                        SetPreviewBackBuffer(MouthPreviewA, MouthPreviewB, _mouthShowA, frame);
+                        _mouthPendingSwap = true;
+                    });
+                }
+            }
+            else
+                _lastMouthFrameLen = 0;
+        }
+
+        private static void SetPreviewBackBuffer(Image imageA, Image imageB, bool showA, byte[] jpegBytes)
+        {
+            try
+            {
+                var back = showA ? imageB : imageA;
+                back.Source = ImageSource.FromStream(() => new MemoryStream(jpegBytes));
+            }
+            catch { }
+        }
+
+        private static void SwapPreviewVisibility(Image imageA, Image imageB, ref bool showA)
+        {
+            showA = !showA;
+            imageA.IsVisible = showA;
+            imageB.IsVisible = !showA;
+        }
+#endif
 
         private async void OnRefreshCamerasClicked(object sender, EventArgs e)
         {
@@ -101,18 +211,21 @@ namespace FacialCameraBroadcaster
                 LeftEyeStreamLabel.Text = running ? url : "";
                 LeftEyeStartButton.IsEnabled = !running;
                 LeftEyeStopButton.IsEnabled = running;
+                LeftEyePreviewFrame.IsVisible = running;
             }
             else if (slot == "Right Eye")
             {
                 RightEyeStreamLabel.Text = running ? url : "";
                 RightEyeStartButton.IsEnabled = !running;
                 RightEyeStopButton.IsEnabled = running;
+                RightEyePreviewFrame.IsVisible = running;
             }
             else
             {
                 MouthStreamLabel.Text = running ? url : "";
                 MouthStartButton.IsEnabled = !running;
                 MouthStopButton.IsEnabled = running;
+                MouthPreviewFrame.IsVisible = running;
             }
         }
 
@@ -171,6 +284,5 @@ namespace FacialCameraBroadcaster
             clearRefs();
             setStreamState(slot, false, port);
         }
-#endif
     }
 }
